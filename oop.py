2878e67daee80d6007616404bd7615da978ff600
jinjin <hsiehjinjin516@gmail.com>
2020-06-13 23:04:06 +0800

    second

--- oop.py
+++ oop.py
@@ -1,3 +1,4 @@
+#---- class ----#
 import random
 class myclass(object):
     var = 10
@@ -12,13 +13,6 @@ class myclass(object):
     def randomnum(self):
         self.num = random.randint(1,10)
 
-class Date(object):  #inherited -> parent / base / super class
-    def get_date(self):
-        return "date"
-class Time(Date):  #inheriting -> child / derived / sub class
-    def get_time(self):
-        return "time"
-
 this_object = myclass() #call __init__
 
 print(this_object.var)
@@ -28,9 +22,42 @@ this_object.get_test()
 this_object.randomnum()
 print(this_object.num)
 
-dt = Date()
+#---- inheritance ----#
+
+class Date(object):  #inherited -> parent / base / super class
+    def __init__(self,name):
+        self.name = name
+    def get_date(self):
+        return "date"
+class Time(Date):  #inheriting -> child / derived / sub class
+    def __init__(self,name):
+        super(Time,self).__init__(name)
+    def get_time(self):
+        return "time"
+
+dt = Date("b")
 print(dt.get_date())
 
-tm = Time()
+tm = Time("a")
 print(tm.get_date())
 print(tm.get_time())
+print(tm.name)
+
+#---- multiple inheritance ----#
+class A(object):
+    def dothis(self):
+        print("dothis in A")
+
+class B(A):
+    pass
+
+class C(object):
+    def dothis(self):
+        print("dothis in C")
+
+class D(B,C):
+    pass
+
+d_instance = D()
+d_instance.dothis()  # depth first search, go to A
+print(D.mro()) #D B A C
